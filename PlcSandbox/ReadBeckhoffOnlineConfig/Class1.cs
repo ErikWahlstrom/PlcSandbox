e2929c3d29c53cb146de0f15ee2a176eaf62553c
ErikWahlstrom <ew@kth.se>
2018-02-20 13:08:21 +0100

    StyleCop

--- PlcSandbox/ReadBeckhoffOnlineConfig/Class1.cs
+++ PlcSandbox/ReadBeckhoffOnlineConfig/Class1.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ReadBeckhoffOnlineConfig
+﻿namespace ReadBeckhoffOnlineConfig
 {
+    using System;
     using System.Diagnostics;
+    using System.Linq;
     using System.Threading;
     using NUnit.Framework;
     using TcEventLoggerAdsProxyLib;
